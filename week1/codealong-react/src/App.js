7c124ebcba40420cd4102d27b875ae205261a84d
nbayram <nbayram4@gmail.com>
2020-01-14 16:49:57 +0100

    Add Project cdn and Button Component

---
+++ week1/codealong-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from './Components/Button.js';
+
+function App() {
+  return (
+    <div className="App" id="quote-box">
+      <Button buttonDisplayName='Next Quote' />
+    </div>
+  );
+}
+
+export default App;
